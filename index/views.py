a03e41013c43bfab65ac475a73cbeecbafc100f2
iam-saitama <aidapdiamond@gmail.com>
2025-01-22 00:58:42 +0500

    views.py updated

--- index/views.py
+++ index/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import NewsCategory, News
+
 # Create your views here.
+# Главная страница
+def home_page(request):
+    # Достаем данные из БД
+    categories = NewsCategory.objects.all()
+    news = News.objects.all()
+    # Передаем данные на frontend
+    contex = {'categories': categories, 'news': news}
+
+    return render(request, 'home.html', contex)
