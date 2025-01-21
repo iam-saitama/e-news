216f87fb362345645d5f066de101865db9165c99
iam-saitama <aidapdiamond@gmail.com>
2025-01-22 00:39:03 +0500

    first

---
+++ index/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+# Таблица категорий
+class NewsCategory(models.Model):
+    title = models.CharField("Название категории", max_length=64)
+    created_at = models.DateTimeField("Дата добавления", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+# Таблица новостей
+class News(models.Model):
+    title = models.CharField("Заголовок", max_length=256)
+    content = models.TextField("Основной текст")
+    category = models.ForeignKey(
+        NewsCategory,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField("Дата добавления", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
